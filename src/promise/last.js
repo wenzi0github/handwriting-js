a4fdc598cd9387dab55f70bc0363ca2f831aa48a
蚊子 <beibeijingfox@foxmail.com>
2022-09-05 13:59:55 +0800

    add last function in Promise (#3)
    * feat(promise): add promise
    
    * chore(test): add jest to test
    
    * chore(test): add jest to test
    
    * chore(test): add jest to test
    
    * feat(promise): add first in promise
    
    * feat(promise): add last in promise
    
    Co-authored-by: beibeijingfox@foxmail.com <shishaobing(师少兵)>

--- src/promise/last.js
+++ src/promise/last.js
@@ -1,1 +1,23 @@
 // get the last fulfilled or rejected promise
+
+Promise.last = (promiseList) => {
+  return new Promise((resolve, reject) => {
+    let num = 0;
+    let len = promiseList.length;
+    let lastResolvedResult;
+
+    const fn = () => {
+      if (++num === len) {
+        lastResolvedResult ? resolve(lastResolvedResult) : reject('all promises rejected');
+      }
+    };
+    promiseList.forEach((pms) => {
+      Promise.resolve(pms)
+        .then((res) => {
+          lastResolvedResult = res;
+          fn();
+        })
+        .catch(fn);
+    });
+  });
+};
